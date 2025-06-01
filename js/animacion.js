7f1dde724f40768a9bc5d4d302f19b727c1cb93a
Unli1401 <unlimited1401@gmail.com>
2025-05-31 20:18:36 -0400

    Se modifican algunos estilos, se agrega texto a la animación

--- js/animacion.js
+++ js/animacion.js
@@ -1,19 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
     const profesiones = [
-        "Analista Programador",
-        "Desarrollador Frontend",
-        "Apasionado por la tecnología",
-        "Me encanta la electrónica",
-        "Actualmente trabajando como Jefe de Bodega",
-        "Administro 3 bodegas de la marca HP",
+        "Analista Programador.",
+        "Desarrollador Frontend.",
+        "Tengo conocimientos en:",
+        "<i> HTML,</i>",
+        "<i>CSS,</i>",
+        "<i>JavaScript,</i>",
+        "<i>MySQL,</i>",
+        "<i>Python,</i>",
+        "<i>SQL,</i>",
+        "<i>Excel Avanzado.</i>",
+        "Me encanta la electrónica y la programación.",
+        "Actualmente trabajando como Jefe de Bodega.",
+        "Administro 3 bodegas de la marca HP.",
+        "Aficionado a la Formula 1.",
+        "Aficionado a la música.",
+        "Apasionado por la tecnología."
+        
+      
+        
     ];
     let i = 0;
 
     function cambiarProfesion() {
-        document.getElementById("profesion").textContent = profesiones[i];
+        /*document.getElementById("profesion").textContent = profesiones[i];*/
+        document.getElementById("profesion").innerHTML = profesiones[i];
         i = (i + 1) % profesiones.length;
     }
-    setInterval(cambiarProfesion, 2200);
+    setInterval(cambiarProfesion, 1200);
 
     const contactoBtn = document.getElementById("contactoBtn");
     const contactoModal = document.getElementById("contactoModal");
